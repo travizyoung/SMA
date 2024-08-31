c780e6ec4244694851da1b4457eb0436328248a7
Yihui <travisyoungdev@outlook.com>
2024-08-31 16:04:49 +0800

    Update Profile Component

--- screens/Profile/Profile.tsx
+++ screens/Profile/Profile.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {Image, SafeAreaView, ScrollView, View, Text} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
+import style from './style';
 
-const Profile = ({navigation}: {navigation: any}) => {
+const Profile = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text style={{color: 'blue'}}>Go Back</Text>
-      </TouchableOpacity>
-      <Text>Welcome to Profile Page</Text>
+      <ScrollView contentContainerStyle={globalStyle.flex}>
+        <View style={style.profileImageContainer}>
+          <View style={style.profileImageContent}>
+            <Image
+              style={style.profileImage}
+              source={require('../../assets/images/default_profile.png')}
+            />
+          </View>
+        </View>
+        <Text style={style.userName}>Yihui Zuo</Text>
+        <View style={style.statContainer}>
+          <View>
+            <Text style={style.statAmount}>45</Text>
+            <Text style={style.statType}>Following</Text>
+          </View>
+          <View style={style.statBorder} />
+          <View>
+            <Text style={style.statAmount}>30M</Text>
+            <Text style={style.statType}>Followers</Text>
+          </View>
+          <View style={style.statBorder} />
+          <View>
+            <Text style={style.statAmount}>100</Text>
+            <Text style={style.statType}>Posts</Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
